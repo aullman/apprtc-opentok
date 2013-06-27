bb7d6e2b6c25ec43bf433c3dfe454daa18bc3429
Adam Ullman <adam@tokbox.com>
2013-06-27 16:05:20 +1000

    Make it redirect you to the /s/:sessionId URL.

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', routes.index);
+app.get('/s/:sessionId', routes.session);
 
 app.listen(8080, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
--- routes/index.js
+++ routes/index.js
@@ -6,19 +6,23 @@ var OPENTOK_API_SECRET = "358115ac4c092a604d3ef2fd615d5b6fb3f5a2e0";
 var ot = new opentok.OpenTokSDK(OPENTOK_API_KEY,OPENTOK_API_SECRET);
 
 exports.index = function(req, res) {
-    var renderIndex = function renderIndex(sessionId) {
-        var token = ot.generateToken({
-          'role': "publisher"
-        });
+    ot.createSession('', {}, function(sessionId) {
+        res.redirect('/s/' + sessionId);
+    });
+};
 
-        res.render('index', {
-            apiKey: OPENTOK_API_KEY,
-            sessionId: sessionId,
-            token: token,
-            host: req.headers.host
-        });
-    };
+exports.session = function(req, res) {
+    var sessionId = req.params.sessionId;
     
-    var sessionId = req.param("s", "");
-    sessionId ? renderIndex(sessionId) : ot.createSession('', {}, renderIndex);
+    var token = ot.generateToken({
+        sessionId: sessionId,
+        role: "publisher"
+    });
+
+    res.render('index', {
+        apiKey: OPENTOK_API_KEY,
+        sessionId: sessionId,
+        token: token,
+        link: "http://" + req.headers.host + "/s/" + sessionId
+    });
 };
